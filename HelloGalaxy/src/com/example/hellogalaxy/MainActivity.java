8d16d3f273f1aba845e20eaf564df5002d18162e
student <student@PC111S31.fgakusei.local>
2014-05-22 14:34:48 +0900

    aaa

--- HelloGalaxy/src/com/example/hellogalaxy/MainActivity.java
+++ HelloGalaxy/src/com/example/hellogalaxy/MainActivity.java
@@ -1,10 +1,11 @@
 package com.example.hellogalaxy;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements View.OnClickListener{
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +18,11 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+
+	@Override
+	public void onClick(View v) {
+
 	}
 
 }
